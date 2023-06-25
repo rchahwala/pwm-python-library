ecf3b22c61a9582bcf1195391fac93d75e2c7764
rchahwala <chahwala.rahul@gmail.com>
2023-06-25 13:55:44 -0700

    Update groupBy_dict.py
    code clean up

--- groupBy_dict.py
+++ groupBy_dict.py
@@ -27,54 +27,49 @@ class FormatData:
     def process_data(self):
 
         # loop through data
-        if len(self.data) > 0:
+        for idx, record in enumerate(self.data):
+            # check if result dic is empty
+            if self.get_result_length() == 0:
+                self.result.append({
+                    "date": str(record["date"]),
+                    "S": int(record["S"]) if "S" in record else 0,
+                    "PNP": int(record["PNP"]) if "PNP" in record else 0,
+                    'F': int(record["F"]) if "F" in record else 0,
+                    "id": idx + 1
+                })
+                # add unique date in temp
+                self.unique_category.append(
+                    str(record["date"])
+                )
+                continue
 
-            for idx, record in enumerate(self.data):
+            # check if result set is not empty
+            if self.get_result_length() > 0:
 
-                # idx = idx + 1
+                # check if date is already present in the temp variable
+                if str(record["date"]) in self.unique_category:
 
-                # check if result dic is empty
-                if self.get_result_length() == 0:
-                    self.result.append({
+                    # get index from result
+                    dt_idx = self.unique_category.index(str(record["date"]))
+                    self.result[dt_idx]["S"] = self.result[dt_idx]["S"] + int((record["S"]) if "S" in record else 0)
+                    self.result[dt_idx]["PNP"] = self.result[dt_idx]["PNP"] + int((record["PNP"]) if "PNP" in record else 0)
+                    self.result[dt_idx]["F"] = self.result[dt_idx]["F"] + int((record["F"]) if "F" in record else 0)
+                    continue
+
+                else:
+
+                    self.result.append( {
                         "date": str(record["date"]),
                         "S": int(record["S"]) if "S" in record else 0,
                         "PNP": int(record["PNP"]) if "PNP" in record else 0,
                         'F': int(record["F"]) if "F" in record else 0,
-                        "id": idx + 1
+                        "id": ++idx
                     })
-                    # add unique date in temp
-                    self.unique_category.append(
-                        str(record["date"])
-                    )
-                    continue
 
-                # check if result set is not empty
-                if self.get_result_length() > 0:
-
-                    # check if date is already present in the temp variable
-                    if str(record["date"]) in self.unique_category:
-
-                        # get index from result
-                        dt_idx = self.unique_category.index(str(record["date"]))
-                        self.result[dt_idx]["S"] = self.result[dt_idx]["S"] + int((record["S"]) if "S" in record else 0)
-                        self.result[dt_idx]["PNP"] = self.result[dt_idx]["PNP"] + int((record["PNP"]) if "PNP" in record else 0)
-                        self.result[dt_idx]["F"] = self.result[dt_idx]["F"] + int((record["F"]) if "F" in record else 0)
-                        continue
-
-                    else:
-
-                        self.result.append( {
-                            "date": str(record["date"]),
-                            "S": int(record["S"]) if "S" in record else 0,
-                            "PNP": int(record["PNP"]) if "PNP" in record else 0,
-                            'F': int(record["F"]) if "F" in record else 0,
-                            "id": ++idx
-                        })
-
-                    # add unique date in temp
-                    self.unique_category.append(
-                        str(record["date"])
-                    )
+                # add unique date in temp
+                self.unique_category.append(
+                    str(record["date"])
+                )
 
 
 result = FormatData(data)
