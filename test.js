9dc857d8c076e2b0c7895c4210602811ab3896bd
rchahwala <chahwala.rahul@gmail.com>
2023-06-26 10:27:32 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const  data = [
   {name: "Rahul", surname: "Chahwala"},
+  {name: "testName3", surname: "test"},
   {name: "Rhythm", surname: "Chahwala"},
   {name: "Riaan", surname: "Chahwala"},
   {name: "Kruti", surname: "Rana"},
@@ -14,37 +15,28 @@ const output = [
   {Rana: ["Kruti"]}
 ]
 
-const process = (list) => {
+const groupBy = (list) => {
 
     const result = []
     const temp = {}
     let idx = 0
 
     for(const item of list) {
-      if (result.length === 0) {
-        result.push({
-          [item.surname]: [item.name]
-        })
-        temp[item.surname] = idx
+      // check if surname is present in temp
+      if(temp[item.surname] >= 0) {
+        result[temp[item.surname]][item.surname].push(item.name)
         continue
       }
 
-      if (result.length > 0) {
-        // check if surname is present in temp
-        if(temp[item.surname] >= 0) {
-          result[temp[item.surname]][item.surname].push(item.name)
-          continue
-        }
-        else {
-          result.push({
-            [item.surname]: [item.name]
-          })
-          temp[item.surname] = ++idx;
-        }
+      if(!temp[item.surname]) { 
+        result.push({
+          [item.surname]: [item.name]
+        })
+        temp[item.surname] = idx++;
       }
     } // for end
     return result
 }
 
-const a = process(data);
-console.log(a)
+const a = groupBy(data);
+console.log(JSON.stringify(a,undefined,3))
